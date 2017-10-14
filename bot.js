ef740c7f578d48cd851faa2ee56fbde7dbd773b7
ssalogel <bibtownstream+ssal@gmail.com>
2017-10-13 20:11:21 -0400

    style, parse-revamp, say/nickname logs, !rollfix

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
-//var score = require('./store/Score.json');
 const store = require('json-fs-store')();
-//var idList = require('./store/Identifier.json');
+var log = require('./store/log.json');
 const urban = require('urban');
 var scoreI = 0;
 var DB = require('./store/Bot_and_User_Info.json');
@@ -19,41 +18,36 @@ function user(username){
     this.username = username;
     this.score = 0;
     this.botNick = username;
-    this.role = "";
+    this.role = "Online";
 }
 
 client.on('guildMemberAdd', member => {
-    var person = new user(member.user.username);
-    DB[member.id] = person;
-    member.addRole("368261968685039636");
-    saveDB();
-    console.log('new person!');
+    if (!DB[member.id]){
+        var person = new user(member.user.username);
+        DB[member.id] = person;
+        member.addRole("368260553690316801");
+        saveDB();
+        console.log('new person!');
+    }
 }); 
- 
+
 //sanitize input
 function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
-
-function answer(id,name){
-  if(DB[id].botNick){
-    return DB[id].botNick;
-  }else return name;
+//return the botNickname or the username if there isn't any
+function answer(id){
+    if(DB[id].botNick){
+        return DB[id].botNick;
+    }else return DB[id].username;
 }
-
+//adds the botNickname for that user
 function addID(id,name){
-  DB[id].botNick = name;
-  store.add(DB, function(err){ if (err) throw err;})
-  return;
+    DB[id].botNick = name;
+    store.add(DB, function(err){ if (err) throw err;})
+    return;
 }
-
-
-
-/*function reload(){
-    score = require('./store/Score.json');
-    console.log('reloading score');
-}*/ //figure out how to make it do that, right now it doesn't seem to want to load from file, only save to once it did the loading on line4
-
+//adds 1 pts to that user
 function addScore(id){ 
     DB[id].score += 1;
     scoreI++;
@@ -63,228 +57,218 @@ function addScore(id){
     }
     return;
 }
-
+//fills the DB w/ new users it missed
 function updateUsers(members, numbers){
     console.log('start');
     for (i = 0; i < numbers; i++){
-        
-        //DB[members[i].user.id].role = members[i].hoistRole.name;
+        if(!DB[members[i].user.id]){
+            DB[members[i].user.id] = new user(members[i].user.username);
+        }
     } 
     saveDB();
-    console.log('end');
 }
-
+//save changes to the DB
 function saveDB(){
     store.add(DB, function(err){ if (err) throw err;});
     console.log('saving');
+    return;
 }
-
+//returns the user's score
 function myScore(id){
     console.log(id, DB[id], DB['bot']);
     return DB[id].score;
 }
-
+//sorts every one's score (if score > 0)  
+// NB: fix it to use for(var key in DB) instead of retrieving user list
 function sortScore(member, numbers){
     var arr = [];
+//    arr = DB[key];
+    console.log(arr[0],arr[1],arr[2]);
+    arr = [];
     for (i = 0; i < numbers; i++){
         if(DB[member[i].user.id].score > 0){
             arr.push([DB[member[i].user.id].username, DB[member[i].user.id].score])
         }
     }
-    /*for (var name in score){
-        arr.push([name, score[name]]);
-    }*/
     arr.sort(function(a,b){return b[1] - a[1]});
-    //console.log(arr);
     return arr;
 }
-
-function parseArgs(message, prefix) {
-  command = message.slice(prefix.length);
-  //remove anything after a space from command
-  if(command.indexOf(' ') !== -1) {
-    command = command.slice(0, command.indexOf(' '));
-  }
-  args = message.splice(prefix.length, message.length);
-  return {
-    command: command,
-    args: args,
-  }
-}
-
-
-
-client.on('message', message => {
-  //prevent bot from answering bots
-  if (message.author.bot) return;
-  //making sure the message was to the bot
-//  console.log("recieved message: '" + message.content + "'");
-
-  let command;
-  let args; 
-  //Prefix set, no name set, starts with prefix
-  
-  addScore(message.author.id);
-  
-  if(settings.prefix) {
-    if(!message.content.startsWith(settings.prefix)) {
-      return;
-    } else {
-      command = message.content.substr(0,message.content.indexOf(' ')).substring(1);
-      args = message.content.substr(message.content.indexOf(' ')+1);
-      if (!command) {command = args.substring(1);}
-    }
-  } 
-
-  if(command.startsWith("!")) return;
-  if (command === 'ping'){
-      message.channel.send('Pong!');
-  }
-  else  if (command === 'say'){
-      message.delete();
-      message.channel.send(args);
-  }
-  else  if (command === 'source'){
-      message.channel.send('https://github.com/bibtown-ssal/DiscordBot/');
-  }
-  else  if (command === 'hug'||command === 'hugs'||command === 'HUG'||command === 'HUGS'||command === 'Hugs'||command === 'Hug'){
-      if(args === '!hug'||args === '!hugs'||args === '!HUG'||args === '!HUGS'||args === '!Hugs'||args === '!Hug'){	  
-        message.channel.send('*hugs ' + answer(message.author.id,message.author.username) + ' back*');	 
-       }  else{
-           message.delete();
-           message.channel.send('*hugs ' + args + '*');  
-       }
-  } 
-  else if (command === 'selfie'){
-      message.channel.send("http://i.imgur.com/dryL4OG.jpg");
-  }
-  else if (command === 'test'){
-    message.channel.send(answer(message.author.id,message.author.username));
-    //console.log(DB[message.author.id].username);
-  }  
-  else if (command === 'callme'){
-    addID(message.author.id, args);
-    message.channel.send('I will now call you: ' + answer(message.author.id,message.author.username));
-  } 
-  else if (command === 'coin'){
-    let x = Math.floor((Math.random() * 10) / 5);
-    let coin = ['head', 'tail'];
-      message.channel.send(coin[x] + ', ' + answer(message.author.id,message.author.username) + ', I hope its the result you wanted!');
-  }
-  else if (command === 'nickname'){
-      if(args != '!nickname' /*&& message.author.id == '256642313550430220'*/){
-        DB.bot.lastname = DB.bot.botNick;
-        message.channel.send("I'm changing my nickname from '" + DB.bot.lastname +"' to '" + args + "' thanks to " + answer(message.author.id,message.author.username));
-        message.guild.member(client.user).setNickname(args);
-        console.log(args + ' ' + message.author.username);
-        addID('bot',args);
-      }
-  }
-  else if (command === 'choose'){
-    console.log('choosing');
-    let x = Math.floor(Math.random() * args.split(' ').length);
-    message.channel.send("I will decide for you, "+answer(message.author.id,message.author.username)+"!\n" + args.split(' ')[x]+"!");
-  } 
-  else if (command === 'help') {
-      message.channel.send("!ping \n!coin : coinflip \n!choose option1 option2 option3 ... optionx : chooses for you!\n!roll #d#+# : will roll the dice combination!\n!nickname aName : nickname me!\n!temp ##c OR ##f :  will convert it to the other temperature scale\n!callme new name : change what the bot calls you!\n!urban word : gives you the urban dictionnary definition of a word\n!hug : give hug\n!say something : makes the bot say something\n!score : gives your score!\n!selfie = self explanatory")
-  } 
-  else if (command === 'temp') {
-    let pattern = /(-?[0-9]+)([cf])/i;
-    if (pattern.test(args)){
-      let tempTo = pattern.exec(args);
-      tempTo[1] = parseInt(tempTo[1]);
-      if (tempTo[2] === 'c'||tempTo[2] === 'C'){
-        message.channel.send(tempTo[0] + ' is ' + Math.round(tempTo[1]*1.8+32) + '°F! (~' + Math.round(tempTo[1]+273) + 'K)');
-      } 
-  else if (tempTo[2] === 'f'||tempTo[2] === 'F'){
-        message.channel.send(tempTo[0] + ' is ' + Math.round((tempTo[1]-32)/1.8) + '°C! (~' + Math.round((tempTo[1]+459)/1.8) + 'K)')
-      }
-    } else message.channel.send('I don\'t understand :(  Please ask me about ##C or ##F');
-  } 
-  else if (command === 'roll'){
-    let pattern = /[0-9]+[d][0-9]+[\+|-]?[0-9]*/i;
+//takes #d#+# as an arg and returns the results in a pre-set string
+function roll(args){
+    let pattern = /[0-9]+[d][0-9]+[\+|-]?[0-9]*/i; //tests if the incoming string is setted properly
     if(pattern.test(args)){
-      let diceReg = /([0-9]+)[d]([0-9]+)([\+|-]?)([0-9]*)/i;
-      let diceArray = diceReg.exec(args);
-      diceArray[1] = parseInt(diceArray[1]);
-      diceArray[2] = parseInt(diceArray[2]);
-      let n = 0;
-      let results = [];
-      for (i = 0; i < diceArray[1]; i++) {
-        results.push(Math.floor(Math.random()*diceArray[2]) + 1);
-      }
-      var total = 0;
-      for(i=0; i < results.length; i++){
-        total += results[i];
-      }
-      if (diceArray[3] === '+'){
-        total += diceArray[4];
-        message.channel.send("You rolled: " + total + ' !  (' + diceArray[0] + ' = ' + results.join(' + ') + ', +' + diceArray[4] + ')');
-      }else if (diceArray[3] === '-'){
-        total -= diceArray[4];
-        message.channel.send("You rolled: " + total + ' !  (' + diceArray[0] + ' = ' + results.join(' + ') + ', -' + diceArray[4] + ')');
-      }else
-      message.channel.send("You rolled: " + total + ' !  (' + diceArray[0] + ' = ' + results.join(' + ') + ')');
-    } 
-  else if(!pattern.test(dice)) {message.channel.send('I don\'t understand :(  Please tell me to roll "#d#+#"!')}
-  } 
-  else if (command === 'setgame'){
-    if(!args){
-      args = null;
-    }
-      client.user.setGame(args);
-  } 
-  else if (command === 'urban'){
-    let definition = urban(args);
-    definition.first(function(json){
-        try{message.channel.send(args + ' \: ' + json.definition);}catch(err){message.channel.send(args + ' is not a defined word in urbandictionnary');}
-    });
+        let diceReg = /([0-9]+)[d]([0-9]+)([\+|-]?)([0-9]*)/i; //sets-up the regex to capture the relevant data
+        let diceArray = diceReg.exec(args); //captures all the relevant bits in an Array(1d2+3,1,2,+,3)
+        diceArray[1] = parseInt(diceArray[1]);
+        diceArray[2] = parseInt(diceArray[2]);  //puts the stringed numbers back in int for math
+        diceArray[4] = parseInt(diceArray[4]);
+        let n = 0;
+        let results = [];
+        for (i = 0; i < diceArray[1]; i++) {
+            results.push(Math.floor(Math.random()*diceArray[2]) + 1); //puts in an array each diceroll
+        }
+        var total = 0;
+        for(i=0; i < results.length; i++){
+            total += results[i];    //sums up the dicerolls
+        }
+        if (diceArray[3] === '+'){ //if we add to the rolls
+            total += (diceArray[4]);
+            return "You rolled: " + total + " !  (" + diceArray[0] + " = " + results.join(' + ') + " + " + diceArray[4] + ")";
+        }else if (diceArray[3] === '-'){ //if we substract to the rolls
+            total -= (diceArray[4]);
+            return "You rolled: " + total + ' !  (' + diceArray[0] + ' = ' + results.join(' + ') + ' - ' + diceArray[4] + ')';
+        }else //rolls w/o modification
+            return "You rolled: " + total + ' !  (' + diceArray[0] + ' = ' + results.join(' + ') + ')';
+        } 
+    else {message.channel.send('I don\'t understand :(  Please tell me to roll "#d#+#"!')} //if the string wasn't set properly
+} 
 
-  } 
-  else if (command === 'score'){
-    message.channel.send(answer(message.author.id,message.author.username) + " : " + myScore(message.author.id));
-  }
-  else if (command === 'reset'){
-    saveScore();
-  }
-  else if(command === 'users'){
-      members = message.guild.members.array();
-      updateUsers(members,message.guild.memberCount);
-  }
-  else if (command === 'rank'){
-      scoreArr = sortScore(message.guild.members.array(),message.guild.memberCount);
-      var top = parseInt(args);
-      if (isNaN(top)){top = 5;}
-      else if (top > scoreArr.length){
-          top = scoreArr.length;
-      }
-      var mess = "";
-      var i = 0;
-      var j = i+1;
-      for (i; i < top/2; i++){
-          j = i+1;
-          mess += j;
-          mess += ". ";
-          mess += scoreArr[i][0];
-          mess += " : ";
-          mess += scoreArr[i][1];
-          mess += "\n";
-      }
-      var mess2 = "";
-      message.channel.send(mess);
-            for (i; i < top; i++){
-          j = i+1;
-          mess2 += j;
-          mess2 += ". ";
-          mess2 += scoreArr[i][0];
-          mess2 += " : ";
-          mess2 += scoreArr[i][1];
-          mess2 += "\n";
-      }
-      message.channel.send(mess2);
-  }
-  else {
-    message.channel.send("I don't understand what you just asked. If you meant to ask me something, type \"!help\" to see how to ask me things.");
-  }
+function logged(name,text,com){
+    var d = new Date();
+    log[d.toString().slice(0,24)] = com + ", " +name + " : " + text;
+    store.add(log, function(err){ if (err) throw err;});
+    console.log('logged');
+    return;
+} 
+   
+client.on('message', message => {
+    //prevent bot from answering bots
+    if (message.author.bot) return;
+    
+    let command;
+    let args; 
+    let arg;
+    addScore(message.author.id);
+  
+    if(!message.content.startsWith(settings.prefix)) { //making sure the message was to the bot
+        return;
+    } else {
+        args = message.content.slice(settings.prefix.length).trim().split(/ +/g); //removes the prefix, puts every 'word' of the message in an array(command, arg1, arg2)
+        command = args.shift().toLowerCase(); //removes the first word of the array and returns it, leaving array(arg1, arg2)
+    } 
+    if(command.startsWith("!")) return; //if somebody puts '!!!' as their message, it isn't talking to the bot
+    if (command === 'say'){ //makes the bot say something
+        logged(message.author.username, args.join(" "),"say");
+        setTimeout(function(){message.delete();},50);
+        message.channel.send(args.join(" "));
+    }
+    else  if (command === 'source'){
+        message.channel.send('https://github.com/bibtown-ssal/DiscordBot/');
+    }
+    else  if (command === 'hug'||command === 'hugs'){
+        if(!args){ //if the whole message was '!hugs' 
+            message.channel.send('*hugs ' + answer(message.author.id) + ' back*');	 
+        }  else{ //if the message was '!hug someone
+            setTimeout(function(){message.delete();},50);
+            message.channel.send('*hugs ' + args.join(" ") + '*');  
+        }
+    } 
+    else if (command === 'selfie'){
+        message.channel.send("http://i.imgur.com/dryL4OG.jpg");
+    }
+    else if (command === 'test'){
+        message.channel.send(answer(message.author.id));
+    }  
+    else if (command === 'callme'){
+        addID(message.author.id, args.join(" "));
+        message.channel.send('I will now call you: ' + answer(message.author.id));
+    } 
+    else if (command === 'coin'){
+        let x = Math.floor((Math.random() * 10) / 5);
+        let coin = ['head', 'tail'];
+        message.channel.send(coin[x] + ', ' + answer(message.author.id) + ', I hope its the result you wanted!');
+    }
+    else if (command === 'nickname'){
+        if(args){
+            DB.bot.lastname = DB.bot.botNick;
+            message.channel.send("I'm changing my nickname from '" + DB.bot.lastname +"' to '" + args.join(" ") + "' thanks to " + answer(message.author.id));
+            message.guild.member(client.user).setNickname(args.join(" "));
+            logged(message.author.username,args.join(" "),"rename");
+            addID('bot',args.join(" "));
+        }
+    }
+    else if (command === 'choose'){
+        console.log('choosing');
+        let x = Math.floor(Math.random() * args.length);
+        message.channel.send("I will decide for you, " + answer(message.author.id) + "!\n" + args[x] + "!");
+    } 
+    else if (command === 'help') {
+        message.channel.send("!ping \n!coin : coinflip \n!choose option1 option2 option3 ... optionx : chooses for you!\n!roll #d#+# : will roll the dice combination!\n!nickname aName : nickname me!\n!temp ##c OR ##f :  will convert it to the other temperature scale\n!callme new name : change what the bot calls you!\n!urban word : gives you the urban dictionnary definition of a word\n!hug : give hug\n!say something : makes the bot say something\n!score : gives your score!\n!selfie = self explanatory")
+    } 
+    else if (command === 'temp') {
+        let pattern = /(-?[0-9]+)([cf])/i;
+        if (pattern.test(args[0])){
+            let tempTo = pattern.exec(args[0]);
+            tempTo[1] = parseInt(tempTo[1]);
+            if (tempTo[2] === 'c'||tempTo[2] === 'C'){
+                message.channel.send(tempTo[0] + ' is ' + Math.round(tempTo[1]*1.8+32) + '°F! (~' + Math.round(tempTo[1]+273) + 'K)');
+            } else if (tempTo[2] === 'f'||tempTo[2] === 'F'){
+                message.channel.send(tempTo[0] + ' is ' + Math.round((tempTo[1]-32)/1.8) + '°C! (~' + Math.round((tempTo[1]+459)/1.8) + 'K)')
+            }
+        } else message.channel.send('I don\'t understand :(  Please ask me about ##C or ##F');
+    } 
+    else if (command === 'roll'){
+        message.channel.send(roll(args.join("")))
+    } 
+    else if (command === 'setgame'){
+        if(!args){
+            args = null;
+        }
+        client.user.setGame(args);
+    } 
+    else if (command === 'urban'){
+        let definition = urban(args);
+        definition.first(function(json){
+            try{message.channel.send(args + ' \: ' + json.definition);}catch(err){message.channel.send(args + ' is not a defined word in urbandictionnary');}
+        });
+    } 
+    else if (command === 'score'){
+        message.channel.send(answer(message.author.id) + " : " + myScore(message.author.id));
+    }
+    else if (command === 'reset'){
+        saveScore();
+    }
+    else if(command === 'usersupdate'){
+        members = message.guild.members.array();
+        updateUsers(members,message.guild.memberCount);
+    }
+    else if(command === 'user'){ 
+    
+    }
+    else if (command === 'rank'){
+        scoreArr = sortScore(message.guild.members.array(),message.guild.memberCount);
+        var top = parseInt(args);
+        if (isNaN(top)){top = 5;}
+        else if (top > scoreArr.length){
+            top = scoreArr.length;
+        }
+        var mess = "";
+        var i = 0;
+        var j = i+1;
+        for (i; i < top/2; i++){
+            j = i+1;
+            mess += j;
+            mess += ". ";
+            mess += scoreArr[i][0];
+            mess += " : ";
+            mess += scoreArr[i][1];
+            mess += "\n";
+        }
+        var mess2 = "";
+        message.channel.send(mess);
+        for (i; i < top; i++){
+            j = i+1;
+            mess2 += j;
+            mess2 += ". ";
+            mess2 += scoreArr[i][0];
+            mess2 += " : ";
+            mess2 += scoreArr[i][1];
+            mess2 += "\n";
+        }
+        message.channel.send(mess2);
+    }
+    else {
+        message.channel.send("I don't understand what you just asked. If you meant to ask me something, type \"!help\" to see how to ask me things.");
+    }
 });
 try{ client.login(settings.token);}catch(err){console.log(err);}
